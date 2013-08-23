4487d1fb6ab3247d82fb0e3419dbe1122a5d52cf
Randy Burgess <wrburgess@wrburgess.com>
2013-08-22 22:55:22 -0500

    installing Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DraftServer::Application.config.secret_key_base = 'd38962c31dab58e63463151e4604cb5eee2e98013eded082e8f8bb369932d733b1186ec37decb604d784f98980b80330399abdc9b526564819bca617c62ec1c5'
